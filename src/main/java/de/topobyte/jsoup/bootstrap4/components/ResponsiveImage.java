cab642b62a82d4150a8287fda47a5c258f43bf1e
Sebastian Kürten <sebastian@topobyte.de>
2020-07-26 12:07:00 +0200

    Add class for responsive images

---
+++ src/main/java/de/topobyte/jsoup/bootstrap4/components/ResponsiveImage.java
@@ -0,0 +1,48 @@
+// Copyright 2020 Sebastian Kuerten
+//
+// This file is part of jsoup-bootstrap4.
+//
+// jsoup-bootstrap4 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// jsoup-bootstrap4 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with jsoup-bootstrap4. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.jsoup.bootstrap4.components;
+
+import de.topobyte.jsoup.components.Img;
+
+public class ResponsiveImage extends Img
+{
+
+	public ResponsiveImage()
+	{
+		super();
+		init();
+	}
+
+	public ResponsiveImage(String src)
+	{
+		super(src);
+		init();
+	}
+
+	public ResponsiveImage(String src, String alt)
+	{
+		super(src, alt);
+		init();
+	}
+
+	private void init()
+	{
+		addClass("img-fluid");
+	}
+
+}
